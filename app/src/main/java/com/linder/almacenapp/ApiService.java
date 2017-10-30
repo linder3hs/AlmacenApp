db99f9d5d12037f599d560e53e1ebb6c47f317aa
Linder Hassinger <linderhassinger00@gmail.com>
2017-10-30 13:35:24 -0500

    Servicio Rest

---
+++ app/src/main/java/com/linder/almacenapp/ApiService.java
@@ -0,0 +1,41 @@
+package com.linder.almacenapp;
+
+import java.util.List;
+
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+
+/**
+ * Created by linderhassinger on 10/30/17.
+ */
+
+public interface ApiService {
+
+    String API_BASE_URL = "https://productos-api-linder3hs.c9users.io/";
+
+    @GET("api/v1/productos")
+    Call<List<Producto>> getProductos();
+
+    @FormUrlEncoded
+    @POST("/api/v1/productos")
+    Call<ResponseMessage> createProducto(@Field("nombre") String nombre,
+                                         @Field("precio") String precio,
+                                         @Field("detalles") String detalles);
+    @Multipart
+    @POST("/api/v1/productos")
+    Call<ResponseMessage> createProductoWithImage(
+            @Part("nombre") RequestBody nombre,
+            @Part("precio") RequestBody precio,
+            @Part("detalles") RequestBody detalles,
+            @Part MultipartBody.Part imagen
+    );
+
+
+}
